5e7031ea03777ee008a6d3ee1c1c65561afa293f
Ibson RG Machado <ibsonRafael@users.noreply.github.com>
2017-09-22 19:16:22 -0300

    Update index.js

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,4 +5,14 @@ module.exports = class extends Generator {
   constructor(args, opts) {
     super(args, opts);
     this.option('babel'); // This method adds support for a `--babel` flag
-}};
+  }
+  
+  method1() {
+    this.log('method 1 just ran');
+  }
+
+  method2() {
+    this.log('method 2 just ran');
+  }
+  
+};

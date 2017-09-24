0a5bf86e2f284081927cea711be324fc53691107
Ibson RG Machado <ibsonRafael@users.noreply.github.com>
2017-09-24 19:19:18 -0300

    Removing babel

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,7 +7,7 @@ module.exports = class extends Generator {
   // The name `constructor` is important here
   constructor(args, opts) {
     super(args, opts);
-    this.option('babel'); // This method adds support for a `--babel` flag
+    // this.option('babel'); // This method adds support for a `--babel` flag
   }
   
   initializing() {

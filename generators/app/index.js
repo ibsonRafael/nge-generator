599313b73085bcb9bc3e6c2549c5b07cf023cbfe
Ibson RG Machado <ibsonRafael@users.noreply.github.com>
2017-09-24 19:13:15 -0300

    Update index.js

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,13 +1,18 @@
 var Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
+  
+  var cfg = {}
+
   // The name `constructor` is important here
   constructor(args, opts) {
     super(args, opts);
     this.option('babel'); // This method adds support for a `--babel` flag
   }
   
-  initializing() { }
+  initializing() {
+    this.cfg = this.config.getAll();
+  }
   
   prompting() { }
   
@@ -17,11 +22,13 @@ module.exports = class extends Generator {
   
   writing() {
     this.log('method 1 just ran');
-    this.fs.copyTpl(
-      this.templatePath('interface.ts'),
-      this.destinationPath('public/IName.ts'),
-      { name: 'Name' }
-    );
+    for (var i=0; i<this.cfg.interfaces.length; i++){
+      this.fs.copyTpl(
+        this.templatePath('interface.ts'),
+        this.destinationPath('public/I' this.cfg.interfaces[i].name '.ts'),
+        { cfg: this.cfg.interfaces[i] }
+      );
+    }
   }
 
   conflicts() { }
